f284477ca85259980594cb4c6e5ef402cacc4654
Anggun Dwi Cahyadi <mr.octavianz@gmail.com>
2022-06-16 13:49:52 +0700

    first commit

---
+++ initial-server-build.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+docker network create --driver=overlay traefik-public --attachable=true
+
+export NODE_ID=$(docker info -f '{{.Swarm.NodeID}}')
+docker node update --label-add traefik-public.traefik-public-certificates=true $NODE_ID
+docker node update --label-add portainer.portainer-data=true $NODE_ID
+docker node update --label-add registry.data=true $NODE_ID
+
+export EMAIL=mr.octavianz@gmail.com
+
+export BASE_DOMAIN=yudhadhstr.my.id
+export DOMAIN_TREAFIK=traefik.yudhadhstr.my.id
+export DOMAIN_PORTAINER=portainer.yudhadhstr.my.id
+export DOMAIN_REGISTRY=registry.yudhadhstr.my.id
+export DOMAIN_SWARMPIT=swarmpit.yudhadhstr.my.id
+export DOMAIN_REGISTRY_UI=registry-ui.yudhadhstr.my.id
+
+export WORKDIR=$(pwd)
+echo "workdir = ${WORKDIR}"
+
+mkdir -p devops/traefik/cert
+
+docker-compose -f treafic.yml pull
+docker-compose -f portainer.yml pull
+docker-compose -f swarmpit.yml pull
+docker-compose -f registry.yml pull
+docker stack deploy traefik -c treafic.yml
+docker stack deploy portainer -c portainer.yml
+docker stack deploy swarmpit -c swarmpit.yml
+docker stack deploy registry -c registry.yml
---
+++ prod/whatsapp-deployment.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+
+PASS=kucingITEM123
+USER=anggun
+
+
+
+docker login registry.yudhadhstr.my.id -u "$USER" -p "$PASS"
+sleep 1
+
+echo ""
+echo "Getting latest for image repositories using pull "
+echo -ne '\n'
+
+
+echo "------------- Building docker-stack -------------"
+
+
+export DOMAIN_API=api.yudhadhstr.my.id
+
+docker-compose -f whatsapp-deployment.yml pull && docker stack deploy whatsapp -c whatsapp-deployment.yml --with-registry-auth
+echo -ne '\n'
+
+docker logout registry.yudhadhstr.my.id
+echo "------------- Docker running -------------"
