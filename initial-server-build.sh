83511116ec890a15f3bfa18d252ef98d5b8cc40b
Anggun Dwi Cahyadi <mr.octavianz@gmail.com>
2022-06-22 13:20:37 +0700

    init server update

--- initial-server-build.sh
+++ initial-server-build.sh
@@ -6,17 +6,14 @@ export NODE_ID=$(docker info -f '{{.Swarm.NodeID}}')
 docker node update --label-add traefik-public.traefik-public-certificates=true $NODE_ID
 docker node update --label-add portainer.portainer-data=true $NODE_ID
 docker node update --label-add registry.data=true $NODE_ID
-docker node update --label-add swarmpit.influx-data=true $NODE_ID
-docker node update --label-add swarmpit.db-data=true $NODE_ID
+docker node update --label-add whatsapp.data=true $NODE_ID
 
 export EMAIL=mr.octavianz@gmail.com
-
 export BASE_DOMAIN=yudhadhstr.my.id
-export DOMAIN_TREAFIK=traefik.yudhadhstr.my.id
-export DOMAIN_PORTAINER=portainer.yudhadhstr.my.id
-export DOMAIN_REGISTRY=registry.yudhadhstr.my.id
-export DOMAIN_SWARMPIT=swarmpit.yudhadhstr.my.id
-export DOMAIN_REGISTRY_UI=registry-ui.yudhadhstr.my.id
+export DOMAIN_TREAFIK=traefik.$BASE_DOMAIN
+export DOMAIN_PORTAINER=portainer.$BASE_DOMAIN
+export DOMAIN_REGISTRY=registry.$BASE_DOMAIN
+export DOMAIN_REGISTRY_UI=registry-ui.$BASE_DOMAIN
 
 export WORKDIR=$(pwd)
 echo "workdir = ${WORKDIR}"
@@ -25,9 +22,7 @@ mkdir -p devops/traefik/cert
 
 docker compose -f treafic.yml pull
 docker compose -f portainer.yml pull
-docker compose -f swarmpit.yml pull
 docker compose -f registry.yml pull
 docker stack deploy traefik -c treafic.yml
 docker stack deploy portainer -c portainer.yml
-docker stack deploy swarmpit -c swarmpit.yml
 docker stack deploy registry -c registry.yml
